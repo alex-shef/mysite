63b98a377f0555d9e691a2f14292a688c62f4654
alex-shef <alexei.shef@gmail.com>
2020-04-30 14:19:34 +0300

    The blog with authentication and WYSIWYG Editor "Summernote"

---
+++ blog/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+
+from .models import Post, Comment
+
+
+# class PostAdmin(SummernoteModelAdmin):
+#     summernote_fields = ('content',)
+#
+#
+# admin.site.register(Post, PostAdmin)
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
+    summernote_fields = ('body',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
---
+++ blog/urls.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
+
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    # path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='blog/registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='blog/registration/logged_out.html'), name='logout'),
+    path('account/', views.account, name='account'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        success_url=reverse_lazy('blog:password_change_done'),
+        template_name='blog/registration/password_change_form.html'),
+        name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='blog/registration/password_change_done.html'),
+        name='password_change_done'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        email_template_name='blog/registration/password_reset_email.html',
+        # subject_template_name='blog/registration/password_reset_subject.txt',
+        success_url=reverse_lazy('blog:password_reset_done'),
+        template_name='blog/registration/password_reset_form.html'),
+        name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='blog/registration/password_reset_done.html'),
+        name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('blog:password_reset_complete'),
+        template_name='blog/registration/password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='blog/registration/password_reset_complete.html'),
+        name='password_reset_complete'),
+    ]
